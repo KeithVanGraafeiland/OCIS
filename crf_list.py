d32e76ab856aa06eee6f51bc8ea5a96223df8ee4
Keith VanGraafeiland <kvangraafeiland@esri.com>
2024-10-31 08:02:54 -0400

    added OCIS_Hex_Sample.ipynb and crf_list.py

---
+++ crf_list.py
@@ -0,0 +1,3 @@
+O2 = r'E:\gis_projects\Ocean and Coastal Information System\Raster_Subset\Dissolved_O2_ssp370_2050.crf'
+swt_100m = r'E:\gis_projects\Ocean and Coastal Information System\Raster_Subset\Sea_Water_Temperature_ssp370_100m_2050.crf'
+dic = r'E:\gis_projects\Ocean and Coastal Information System\Raster_Subset\dissolved_inorganic_carbon_ssp370_2050.crf'
